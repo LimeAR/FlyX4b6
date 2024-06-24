896ee7338f714c2f9dddcb26ac316d21312c702a
Stephan-Marie Rubayiza <rubayizastefan@gmail.com>
2024-06-24 11:50:24 +0000

    Lens update

--- src/main.js
+++ src/main.js
@@ -8,7 +8,10 @@ const session = await cameraKit.createSession();
 
 document.getElementById('canvas').replaceWith(session.output.capture);
 
-const {lenses} = await cameraKit.lensRepository.loadLensGroups(['1ab5269b-f2b0-4570-a30f-74a123521727'])
+const lens = await cameraKit.lensRepository.loadLens(
+  '462de4bf-a3ed-4010-adfe-d6116f39d18d',
+  '1ab5269b-f2b0-4570-a30f-74a123521727'
+);
 
 const mediaStream = await navigator.mediaDevices.getUserMedia({
   video:{
@@ -27,6 +30,6 @@ await session.setSource(source);
 
 session.play('capture');
 
-await session.applyLens(lenses[2]);
+await session.applyLens(lens);
 
 })();
